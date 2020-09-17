a1b8a5acc037383758305e1566242064d1c5351d
gsanto <gsanto@appen.com>
2020-09-16 22:28:31 -0300

    numbering is working as expected

--- trabalho1/crossover.py
+++ trabalho1/crossover.py
@@ -1,19 +1,20 @@
 from individual import *
 
+
 def cross(p1, p2):
 
     aux = []
-    
-    #append os 3 primeiros elementos do genoma de p1
+
+    # append os 3 primeiros elementos do genoma de p1
     for i in p1.genome[:3]:
         aux.append(i)
 
-    #append os elementos a partir do terceiro do genoma P2
+    # append os elementos a partir do terceiro do genoma P2
     for i in p2.genome[3:]:
-        #Para cada elemento i do genoma p2, enquanto i ja estiver presente no genoma aux do filho, i++
+        # Para cada elemento i do genoma p2, enquanto i ja estiver presente no genoma aux do filho, i++
         while i in aux and i != aux[0]:
-            i += 1            
-            #i % 9 garante que 0 <= i < 9
+            i += 1
+            # i % 9 garante que 0 <= i < 9
             i %= 9
         aux.append(i)
 
@@ -22,30 +23,76 @@ def cross(p1, p2):
 
     return indiv
 
+
 def crossover(population):
-    
+
     sons = []
-    
-    #cruza os individuos da população
-    #individuo[i] cruza com individuo[i+1]
+
+    # cruza os individuos da população
+    # individuo[i] cruza com individuo[i+1]
     for i in range(0, len(population)-2):
         j = 1
 
-        #evita a reprodução entre dois individuos com mesmo genoma (que resultaria em mais um clone)
-        #se individuo[i] tem mesmo genoma que individuo[i+1], indivudo[i] cruzara com individuo[i+j], 1 < j, ate que sejam diferentes
+        # evita a reprodução entre dois individuos com mesmo genoma (que resultaria em mais um clone)
+        # se individuo[i] tem mesmo genoma que individuo[i+1], indivudo[i] cruzara com individuo[i+j], 1 < j, ate que sejam diferentes
         while(population[i].genome == population[i+j].genome) and (population[i+j] != population[-1]):
-            j+=1
+            j += 1
 
         aux = cross(population[i], population[i+j])
         sons.append(aux)
 
     return sons
-     
+
+
+def cross2(p1, p2):
+
+    aux = []
+
+    # append os 3 primeiros elementos do genoma de p1
+    for i in range(len(p1.genome)):
+        var = random.randint(0, 2)
+        if(var == 0):
+            while p1.genome[i] in aux and p1.genome[i] != aux[0]:
+                p1.genome[i] += 1
+                # i % 9 garante que 0 <= i < 9
+                p1.genome[i] %= 9
+            aux.append(p1.genome[i])
+        else:
+            while p2.genome[i] in aux and p2.genome[i] != aux[0]:
+                p2.genome[i] += 1
+                # i % 9 garante que 0 <= i < 9
+                p2.genome[i] %= 9
+            aux.append(p2.genome[i])
+
+    indiv = Individual(aux)
+    indiv.total_distance = indiv.calculate_total_distance(indiv.genome)
+
+    return indiv
+
+
+def crossover2(population):
+    sons = []
+
+    for i in range(0, len(population)-2):
+        # cruza os individuos da população
+        # individuo[i] cruza com individuo[i+1]
+        for i in range(0, len(population)-2):
+            j = 1
+
+            # evita a reprodução entre dois individuos com mesmo genoma (que resultaria em mais um clone)
+            # se individuo[i] tem mesmo genoma que individuo[i+1], indivudo[i] cruzara com individuo[i+j], 1 < j, ate que sejam diferentes
+            while(population[i].genome == population[i+j].genome) and (population[i+j] != population[-1]):
+                j += 1
+
+        aux = cross2(population[i], population[i+j])
+        sons.append(aux)
+
+    return sons
 ########################################################################################################
 # def crossover(population):
-    
+
 #     sons = []
-    
+
 #     #cruza os individuos da população
 #     #individuo[i] cruza com individuo[i+1]
 #     #individuo[i+1] NAO cruza com individuo [i+2]
--- trabalho1/main.py
+++ trabalho1/main.py
@@ -2,72 +2,85 @@ from individual import *
 from mutate import *
 from crossover import *
 
-#cria populacao de N individuos range(0,N)
+# cria populacao de N individuos range(0,N)
+
+
 def createPopulation():
     population = []
-    for i in range(0,50):
+    for i in range(0, 50):
         aux = Individual()
         population.append(aux)
     return population
 
-#ordena populacao usando a distancia total como parametro
+# ordena populacao usando a distancia total como parametro
+
+
 def sortDist(val):
     return val.total_distance
- 
-#printa a populacao atual
-#[genoma] -> distancia total
+
+# printa a populacao atual
+# [genoma] -> distancia total
+
+
 def print_pop(population):
     for i in range(0, len(population)):
         print(population[i].genome, "->", population[i].total_distance)
+
 
 def print_best(population):
     for i in range(0, 5):
         print(population[i].genome, "->", population[i].total_distance)
 
-#discarda N/2 os piores individuos da população
-#os 50% mais lentos vao ser descartados sem piedade
+# discarda N/2 os piores individuos da população
+# os 50% mais lentos vao ser descartados sem piedade
+
+
 def discard_worst(population):
     i = len(population) - 1
-    while i > len(population)/2 :
+    while i > len(population)/2:
         population.pop()
         i -= 1
 
-#cria populacao
-#ordena os individuos por distancia
-def main():    
+# cria populacao
+# ordena os individuos por distancia
+
+
+def main():
     population = createPopulation()
-    population.sort(key=sortDist)    
+    population.sort(key=sortDist)
 
     print("Population size = ", len(population))
     print_pop(population)
 
     i = 0
-    while i < 1000 :
-        print("========================= GENERATION ", i+1  , "=========================")
-        
-        #cria uma geracao de filhos
+    while i < 1000:
+        print("========================= GENERATION ",
+              i+1, "=========================")
+
+        # cria uma geracao de filhos
         new_gen = crossover(population)
-        #muta geracao filhos
+        # muta geracao filhos
         mutate(new_gen)
-        
+
         # print("--------------------------------")
         # print("new gen")
         # print_pop(new_gen)
         # print("--------------------------------")
-        
-        #descarta os piores sem piedade
+
+        # descarta os piores sem piedade
         discard_worst(population)
-        
-        #junta a populacao dos melhores pais com os novos filhos
+
+        # junta a populacao dos melhores pais com os novos filhos
         population = population + new_gen
-        
-        #ordena
+
+        # ordena
         population.sort(key=sortDist)
 
         # print("Population size = ", len(population))
         # print_pop(population)
         print("TOP 5:")
         print_best(population)
-        i+=1
+        i += 1
+
 
 main()
