6b8975d82c6b0442302b3659811f86241a01f410
Poskvansk <luisfilipesrct@gmail.com>
2020-10-10 18:21:24 -0300

    mais um bocado de regras

---
+++ trabalho2/Pac.py
@@ -0,0 +1,99 @@
+
+class Paciente():
+
+    neonato = 0
+
+    temp_corpo = 0
+    dias_febre = 0
+
+    # manchas = 0
+    mancha_freq = 0
+
+    dor_musc_freq = 0
+
+    dor_art_freq = 0
+    dor_art_intensidade = 0
+
+    edema_art = 0
+    edema_intd = 0
+
+    conjuntivite = 0
+
+    dor_cabeca_freq = 0
+    dor_cabeca_intd = 0
+
+    coceira = 0
+
+    hiptrof_gangli_freq = 0
+    disc_hemo = 0
+    acomet_neuro = 0
+
+def getStatusPaciente(paciente):
+
+    paciente.temp_corpo = float(input("Qual a temperatura corporal? "))
+
+    if paciente.temp_corpo > 38.0:
+        paciente.dias_febre = int(input("Por quantos dias observou-se a febre? "))
+    else:
+        paciente.dias_febre = 0
+
+    # # aux = input("Manchas na pele? (s/n) ")
+    # # if aux[0] == 's': 
+    #     #  paciente.manchas = 1
+    # paciente.manchas_freq = int(input("A partir de quantos dias observou-se as manchas? "))
+
+    # paciente.dor_musc_freq = int(input("De 0 a 10, quão alta é a frequencia da dor muscular? "))
+    
+    # paciente.dor_art_freq = int(input("De 0 a 10, quão alta é a frequencia da dor articular? "))
+    # paciente.dor_art_intd = int(input("De 0 a 10, quão alta é a intensidade da dor articular? "))
+
+    aux = input("Ocorreu edema da articulação? (s/n) ")
+    if aux[0] == 's': 
+        paciente.edema_art = 2
+        paciente.edema_intd = int(input("De 1 a 10, qual a intensidade do edema? "))
+    else:
+        paciente.edema_art = 0
+        paciente.edema_intd = 0
+
+
+    aux = input("Houve conjuntivite? (s/n) ")
+    if aux[0] == 's': 
+        paciente.conjuntivite = 2
+    else:
+        paciente.conjuntivite = 0
+
+    # aux = input("Houve dor de cabeça? (s/n)")
+    # if aux[0] == 's': 
+    #     paciente.dor_cabeca_freq = 1
+
+    # paciente.dor_cabeca_intd = int(input("De 0 a 10, qual a intensidade da dor de cabeça? "))
+
+    paciente.coceira = int(input("De 0 a 10, qual a intensidade da coceira? "))
+
+    # aux = input("Houve hipertrofia posso colocar mais algumas regras, t colocar mais algumas regras, tnar? (s/n) ")
+    # if aux[0] == 's': 
+    #     paciente.hiptrof_gangli_freq = 1
+
+
+    aux = input("Houve discrasia hemorrágica? (s/n) ")
+    if aux[0] == 's': 
+        paciente.disc_hemo = 2
+    else:
+        paciente.disc_hemo = 0
+
+
+    aux = input("Houve acometimento neurológico? (s/n) ")
+    if aux[0] == 's': 
+        paciente.acomet_neuro = 2
+        
+        neo = input("O paciente é neonato? (s/n) ")
+        if neo[0] == 's':
+            paciente.neonato = 2
+        else:
+            paciente.neonato = 0
+
+    else:
+        paciente.acomet_neuro = 0
+
+
+    return paciente
--- trabalho2/main.py
+++ trabalho2/main.py
@@ -2,140 +2,126 @@ import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 import matplotlib.pyplot as plt
-
-class Paciente():
-
-    idade = 0
-
-    temp_corpo = 0
-    dias_febre = 0
-
-    # manchas = 0
-    mancha_freq = 0
-
-    dor_musc_freq = 0
-
-    dor_art_freq = 0
-    dor_art_intensidade = 0
-
-    edema_art = 0
-    edema_art_intd = 0
-
-    conjuntivite = 0
-
-    dor_cabeca_freq = 0
-    dor_cabeca_intd = 0
-
-    coceira = 0
-
-    hiptrof_gangli_freq = 0
-    disc_hemo = 0
-    acomet_neuro = 0
-
-def getStatusPaciente(paciente):
-
-    paciente.temp_corpo = int(input("Qual a temperatura corporal? "))
-    paciente.dias_febre = int(input("Por quantos dias observou-se a febre? "))
-
-    # # aux = input("Manchas na pele? (s/n) ")
-    # # if aux[0] == 's': 
-    #     #  paciente.manchas = 1
-    # paciente.manchas_freq = int(input("A partir de quantos dias observou-se as manchas? "))
-
-    paciente.dor_musc_freq = int(input("De 0 a 10, quão alta é a frequencia da dor muscular? "))
-    
-    # paciente.dor_art_freq = int(input("De 0 a 10, quão alta é a frequencia da dor articular? "))
-    # paciente.dor_art_intd = int(input("De 0 a 10, quão alta é a intensidade da dor articular? "))
-
-    # aux = input("Ocorreu edema da articulação? (s/n) ")
-    # if aux[0] == 's': 
-    #     paciente.edema_art = 1
-
-    # paciente.edema_intd = input("De 0 a 10, qual a intensidade do edema? ")
-
-    # aux = input("Houve conjuntivite? (s/n) ")
-    # if aux[0] == 's': 
-    #     paciente.conjuntivite = 1
-
-    # aux = input("Houve dor de cabeça? (s/n)")
-    # if aux[0] == 's': 
-    #     paciente.dor_cabeca_freq = 1
-
-    # paciente.dor_cabeca_intd = int(input("De 0 a 10, qual a intensidade da dor de cabeça? "))
-
-    # paciente.coceira = input("De 0 a 10, qual a intensidade da coceira? ")
-
-    # aux = input("Houve hipertrofia ganglionar? (s/n) ")
-    # if aux[0] == 's': 
-    #     paciente.hiptrof_gangli_freq = 1
+import matplotlib
 
 
-    # paciente.disc_hemo = input("Houve discrasia hemorrágica? (s/n) ")
-
-    # paciente.acomet_neuro = input("Houve acometimento neurológico? (s/n) ")
-
-    # paciente.idade = int(input("Qual a idade do paciente? "))
-
-    return paciente
+###########
+from Pac import *
+###########
 
 def main():
 
     p1 = Paciente()
     p1 = getStatusPaciente(p1)
 
+    #########
+
     febre = ctrl.Antecedent(np.arange(36, 40.5, 0.5), 'febre')
     dias_febre = ctrl.Antecedent(np.arange(0, 8, 1), 'dias_febre')
+
+    #########
 
     # manchas = 
     # mancha_freq = ctrl.Antecedent(np.arange(0,8), 'dia aparicao mancha')
 
-    dor_musc_freq = ctrl.Antecedent(np.arange(0,11), 'dor_musc_freq')
+    #########
+
+    # dor_musc_freq = ctrl.Antecedent(np.arange(0,11), 'dor_musc_freq')
+
+    #########
 
     # dor_art_freq = ctrl.Antecedent(np.arange(0, 11), 'Freq. dor artic.')
     # dor_art_intensidade = ctrl.Antecedent(np.arange(0, 11), 'Intensidade dor artic.')
 
-    # # edema_art = 0
-    # edema_art_intd = ctrl.Antecedent(np.arange(0, 11), 'Intensdd. edema')
+    #########
 
-    # # conjuntivite = ctrl.Antecedent(np.arange(0,2), 'conjuntivite')
+    edema_art = ctrl.Antecedent(np.arange(0,3), 'edema_art')
+    edema_intd = ctrl.Antecedent(np.arange(1, 11), 'edema_intd')
+
+    #########
+
+    conjuntivite = ctrl.Antecedent(np.arange(0,3), 'conjuntivite')
+
+    #########
 
     # # dor_cabeca_freq = 0
     # dor_cabeca_intd = ctrl.Antecedent(np.arange(0, 11), 'Intendd. dor cabeça')
 
-    # coceira = ctrl.Antecedent(np.arange(0, 11), 'Intdd coceira')
+    #########
 
-    # hiptrof_gangli_freq = 0
-    # disc_hemo = 0
-    # acomet_neuro = 0
+    coceira = ctrl.Antecedent(np.arange(0, 11), 'coceira')
 
-    ################################################
+    #########
 
-    # idade.automf(3)
+    # hiptrof_gangli_freq = ctrl.Antecedent(np.arange(0,2), 'ganglionar')
+
+    #########
+
+    disc_hemo = ctrl.Antecedent(np.arange(0,3), 'disc_hemo')
+
+    #########
+
+    acomet_neuro = ctrl.Antecedent(np.arange(0,3), 'acomet_neuro')
+    neonato = ctrl.Antecedent(np.arange(0,3), 'neonato')
+
+    ############################################### OKOKOKOK
     febre.automf(3)
-    # dias_febre.automf(3)
+
     dias_febre['curta'] = fuzz.trimf(dias_febre.universe, [0, 0, 3])
     dias_febre['media'] = fuzz.trimf(dias_febre.universe, [2, 3, 5])
     dias_febre['longa'] = fuzz.trimf(dias_febre.universe, [4, 7, 7])
-    # dias_febre.view()
-    # input("a")
+
+    ############################################### 
 
     # # mancha.automf(3) 0
     # mancha_freq.automf(3)
 
-    dor_musc_freq.automf(3)
+    ###############################################
+
+    # dor_musc_freq.automf(3)
+
+    ###############################################
 
     # dor_art_freq.automf(3)
     # dor_art_intensidade.automf(3)
 
-    # # edema_art.automf(3)
-    # edema_art_intd.automf(3)
-    # # conjuntivite.automf(3)
+    ############################################### OKOKOKOK
+
+    edema_art['nao'] = fuzz.trimf(edema_art.universe, [0,0,1])
+    edema_art['sim'] = fuzz.trimf(edema_art.universe, [1,2,2])
+
+    edema_intd.automf(3)
+
+    ###############################################
+
     # # dor_cabeca_freq.automf(3)
     # dor_cabeca_intd.automf(3)
-    # coceira.automf(3)
-    # # hiptrof_gangli_freq.automf(3)
-    # # disc_hemo.automf(3)
-    # # acomet_neuro.automf(3)
+
+    ###############################################
+
+    # hiptrof_gangli_freq)
+
+    ############################################### OKOKOKOK
+
+    coceira.automf(3)
+
+    ###############################################
+
+    acomet_neuro['nao'] = fuzz.trimf(disc_hemo.universe, [0,0,1])
+    acomet_neuro['sim'] = fuzz.trimf(disc_hemo.universe, [1,2,2])
+
+    neonato['nao'] = fuzz.trimf(conjuntivite.universe, [0,0,1])
+    neonato['sim'] = fuzz.trimf(conjuntivite.universe, [1,2,2])
+
+    ############################################### OKOKOKOK
+
+    conjuntivite['nao'] = fuzz.trimf(conjuntivite.universe, [0,0,1])
+    conjuntivite['sim'] = fuzz.trimf(conjuntivite.universe, [1,2,2])
+
+    ############################################### OKOKOK
+
+    disc_hemo['nao'] = fuzz.trimf(disc_hemo.universe, [0,0,1])
+    disc_hemo['sim'] = fuzz.trimf(disc_hemo.universe, [1,2,2])
 
     ################################################
     dengue = ctrl.Consequent(np.arange(0, 1.1, 0.1), 'dengue')
@@ -154,6 +140,7 @@ def main():
     chikungunya['alta'] = fuzz.trimf(dengue.universe, [0.5, 1.0, 1.0])
     ################################################
 
+
     rule1 = ctrl.Rule(febre['good'] & dias_febre['longa'], consequent=(dengue['alta'], zika['baixa'], chikungunya['baixa']))
     rule11 = ctrl.Rule(febre['good'] & dias_febre['media'], consequent = (dengue['alta'], zika['baixa'], chikungunya['alta']))
     rule111 = ctrl.Rule(febre['good'] & dias_febre['curta'], consequent = (dengue['baixa'], zika['baixa'], chikungunya['alta']))
@@ -165,15 +152,61 @@ def main():
     rule3 = ctrl.Rule(febre['average'] & dias_febre['longa'], consequent = (dengue['alta'], zika['media'], chikungunya['baixa']))
     rule33 = ctrl.Rule(febre['average'] & dias_febre['media'], consequent = (dengue['media'], zika['media'], chikungunya['baixa']))
     rule333 = ctrl.Rule(febre['average'] & dias_febre['curta'], consequent = (dengue['baixa'], zika['alta'], chikungunya['baixa']))
+    
+    ###############################################
+
+    rule_edema1 = ctrl.Rule(edema_art['nao'], consequent=(dengue['alta'], zika['baixa'], chikungunya['baixa']))
+    rule_edema2 = ctrl.Rule(edema_art['sim'], consequent=(dengue['baixa'], zika['alta'], chikungunya['alta']))
+
+    rule_edema3 = ctrl.Rule(edema_intd['good'], consequent=(zika['baixa'], chikungunya['alta']))
+    rule_edema4 = ctrl.Rule(edema_intd['average'], consequent=(zika['alta'], chikungunya['alta']))
+    rule_edema5 = ctrl.Rule(edema_intd['poor'], consequent=(zika['alta'], chikungunya['media']))
+
+    ###############################################
+
+    rule_coceira = ctrl.Rule(coceira['average'] | coceira['good'], consequent=(zika['alta'], dengue['baixa'], chikungunya['baixa']))
+    rule_coceira2 = ctrl.Rule(coceira['poor'], consequent=(zika['baixa'], dengue['alta'], chikungunya['alta']))
+
+    ###############################################
+
+    rule_conjuntivite = ctrl.Rule(conjuntivite['sim'], consequent=(zika['alta'], chikungunya['media'], dengue['baixa']))
+
+    ###############################################
+
+    rule_hemo = ctrl.Rule(disc_hemo['sim'], consequent=(zika['baixa'], dengue['media'], chikungunya['baixa']))
+
+    ###############################################
+
+    rule_neuro1 = ctrl.Rule(acomet_neuro['sim'], consequent=(dengue['baixa'], zika['media'], chikungunya['baixa']))
+    # rule_neuro2 = ctrl.Rule(acomet_neuro['sim'] , consequent=(dengue['baixa'], zika['media'], chikungunya['baixa']))
+
+    ###############################################
 
     # rule_dm = ctrl.Rule(dor_musc_freq['good'])
 
+
     diag_ctrl = ctrl.ControlSystem([rule1, rule11, rule111, rule2, rule22, rule222,
-                                     rule3, rule33, rule333])
+                                    rule3, rule33, rule333,
+                                    rule_edema1, rule_edema2, rule_edema3, rule_edema4, rule_edema5,
+                                    rule_coceira, rule_coceira2,
+                                    rule_hemo, rule_conjuntivite])
+
     diag_result = ctrl.ControlSystemSimulation(diag_ctrl)
 
     diag_result.input['febre'] = p1.temp_corpo
     diag_result.input['dias_febre'] = p1.dias_febre
+    
+    diag_result.input['edema_art'] = p1.edema_art
+    diag_result.input['edema_intd'] = p1.edema_intd
+
+
+    diag_result.input['coceira'] = p1.coceira
+
+    diag_result.input['disc_hemo'] = p1.disc_hemo
+
+    diag_result.input['conjuntivite'] = p1.conjuntivite
+
+
 
     diag_result.compute()
 
